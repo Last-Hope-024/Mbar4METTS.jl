3c82ca547607a10c893e8dd7ee37909593f16a34
Documenter.jl <documenter@juliadocs.github.io>
2025-07-08 16:20:45 +0000

    build based on 77af4e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Mbar4METTS.jl","page":"Mbar4METTS.jl","title":"Mbar4METTS.jl","text":"","category":"section"},{"location":"","page":"Mbar4METTS.jl","title":"Mbar4METTS.jl","text":"Documentation for Mbar4METTS.jl","category":"page"}]
+}
